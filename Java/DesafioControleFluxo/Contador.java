d161105007ff8ed498094a6a62b27fb11a0fa000
vtmattedi <vtmattedi@gmail.com>
2024-10-21 16:14:09 -0300

    better execption message

--- Java/DesafioControleFluxo/Contador.java
+++ Java/DesafioControleFluxo/Contador.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Contador {
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
 		
 		int contagem = parametroDois - parametroUm;
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm + i);
+        }
 		//realizar o for para imprimir os números com base na variável contagem
 	}
 	public static void main(String[] args) {
@@ -19,7 +25,8 @@ public class Contador {
 			contar(parametroUm, parametroDois);
 		
 		}catch (ParametrosInvalidosException exception) {
-			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            System.out.println(exception.getMessage());
+			//System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
         finally {
             terminal.close();
